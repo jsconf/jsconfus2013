7b254ec494ecd7780fbdcab7fc66c9e877c1a849
Chris Williams <chris@iterativedesigns.com>
2013-05-19 21:39:44 -0400

    Adding schedule.js

---
+++ public/schedule.js
@@ -0,0 +1,191 @@
+var schedule = [{
+  "uid": "JSCONF2013-A100",
+  "title": "Opening Remarks",
+  "description": "An overview of the confernece and a couple key points that will make your JSConf experience worthwhile.",
+  "speaker": {
+    "name": "The Williams Family",
+    "description": "Curators of JSConf US"
+  },
+  "location": "Yammer Track",
+  "starting_at": "2013/05/29 9:30:00",
+  "ending_at": "2013/05/29 10:00:00"
+},
+{
+  "uid": "JSCONF2013-A101",
+  "title": "Lessons from the experimental edge of technology",
+  "description": "When asked if I wanted to build a Chrome Experiment: a thing that shows off some of the new technology coming to browsers - of course I said yes! Then promptly I bit off the biggest bite of tech I could get my hands on: WebRTC, two way video, peer connection API, orientation events, CSS3 animations, Three.js: generally a lot of tech I didn't really understand!\nThis talk is about the experience of working with WebRTC for video & data streaming and creating a game with embedded video. How some of the problems I encountered blew my schedule to pieces and perhaps how I could have done things differently.\nSpoiler: WebRTC may actually be more of a mindf**k than AppCache.",
+  "speaker": {
+    "name": "Remy Sharp ",
+    "description": "@rem"
+  },
+  "location": "Yammer Track",
+  "starting_at": "2013/05/29 10:00:00",
+  "ending_at": "2013/05/29 10:30:00"
+},
+{
+  "uid": "JSCONF2013-B101",
+  "title": "The JavaScript Debug Robot",
+  "description": "Sure, you can use JavaScript to program Robots. But how about having a Robot debug your JavaScript instead? \nDescription: Programming is cool. Developing new features is awesome. Making them work is… boring. In this talk we'll show you how a (software) Robot can make your life more fun by helping debug your code.",
+  "speaker": {
+    "name": "Olivier Crameri",
+    "description": "@oliviercrameri"
+  },
+  "location": "SpringSource Track",
+  "starting_at": "2013/05/29 10:00:00",
+  "ending_at": "2013/05/29 10:30:00"
+},
+{
+  "uid": "JSCONF2013-C101",
+  "title": "Learning Node.js",
+  "description": "A hands-on on-boarding to Node.js focused on teaching you the essentials of how Node.js works thru code: Callback vs. Streams, How node core remains minimal with npm and user-land, What does Node core do for you (focused on network protocols). Finally we put it all together with a sample app and discuss performance implications of real-life Node.js production applications.",
+  "speaker": {
+    "name": "Nuno Job",
+    "description": "@dscape"
+  },
+  "location": "Training Track",
+  "starting_at": "2013/05/29 10:00:00",
+  "ending_at": "2013/05/29 12:30:00"
+},
+
+
+
+
+{
+  "uid": "JSCONF2013-A102",
+  "title": "JavaScript Masterclass",
+  "description": "I am not an expert developer. I am also not a beginning developer.\nI'm an intermediate developer, and I want to navigate the path from being a mediocre or good developer, to becoming a great or expert level developer.\nHow in the heck do I do this?\nThe tutorial ecosystem of the web is heavily skewed towards beginners, but what about the rest of us?\nIf you identify with these statements in the slightest, then this talk is for you. With ideas from cognitive science, education, and some advice from admired programmers along the way, we are going to explore what it means to be a world-class developer and what we need to do to get there. I'll talk a little bit about the philosophy, attitude, and habits (both good and bad) required for learning how to learn better, and then we will talk about the kinds of skills and knowledge that an expert programmer in general needs. Along the way we'll touch about various myths and cognitive biases about who can and cannot be a great programmer, and leave you with a path to get started. By the end of the talk, I will have proposed a curriculum of topics that should take an intermediate developer and turn them into an advanced one, and special attention will be given to mastery of the JavaScript language.",
+  "speaker": {
+    "name": "Angelina Fabbro",
+    "description": "@angelinamagnum"
+  },
+  "location": "Yammer Track",
+  "starting_at": "2013/05/29 10:30:00",
+  "ending_at": "2013/05/29 11:00:00"
+},
+{
+  "uid": "JSCONF2013-B102",
+  "title": "APIs and Making Humankind Better at Programming",
+  "description": "Making humankind better at programming is one of the big ideas, the big problems to solve. Coding is the new literacy and a terrifying number of people in both developed and underdeveloped countries do not know how to program. Worse, enrollment in CS and Engineering programs is dropping, so it seems as if interest itself is waning. What can we do to fix this? ",
+  "speaker": {
+    "name": "Carter Rabasa",
+    "description": "@carterrabasa"
+  },
+  "location": "SpringSource Track",
+  "starting_at": "2013/05/29 10:30:00",
+  "ending_at": "2013/05/29 11:00:00"
+},
+
+
+{
+  "uid": "JSCONF2013-B103",
+  "title": "Surviving the Offline Apocalypse",
+  "description": "Everyone knows the apocalypse is coming, but it's not what you think. It's not zombies or prophesied Mayan destruction. No, my friends, it's the offline apocalypse and it is already here! Billions of devices have already been affected and many more will be affected each and every day! Fortunately, HTML5 provides us with weapons like Application Cache, IndexedDB and the FileSystem API to survive the offline apocalypse and have fun doing so. By walking through the building of an "offline first" JS application that is currently deployed across Africa, you will learn how to use these technologies to build robust offline solutions.",
+  "speaker": {
+    "name": "John Kleinschmidt",
+    "description": "@jkleinsc"
+  },
+  "location": "SpringSource Track",
+  "starting_at": "2013/05/29 11:00:00",
+  "ending_at": "2013/05/29 11:30:00"
+},
+
+
+
+
+{
+  "uid": "JSCONF2013-A104",
+  "title": "JavaScript is Literature is JavaScript",
+  "description": "Anyone who thinks Brendan Eich invented JavaScript has probably never read Oscar Wilde. “The truth is rarely pure and never simple” says Algernon in Wilde's Importance of Being Earnest, thus betraying a wary familairity with our favorite language. It turns out JavaScript's highly expressive nature and the infinite posibilities of its syntax made it a favored medium for our literary forebears.\nLast summer I introduced the concept of Literary JavaScript with If Hemingway wrote JavaScript, wherein five famous writers demonstrated their JavaScript smarts. That article generated very positive reactions from both the JavaScript and Literary communities (including write-ups in the New Yorker, Huffington Post and Wired Magazine) and this talk will build on those themes, drawing parallels between the myriad stylistic facets of JavaScript and the kaleidescope of literary genres. There is a serious aspect - I hope to demonstrate that JavaScript's loose structure and lack of boilerplate encourages creativity and is something to celebrate rather than something to WTF about.\nOh, and as an added bonus I'll be unveiling an additional set of recently re-discovered works of literary JavaScript genius.",
+  "speaker": {
+    "name": "Angus Croll",
+    "description": "@angustweets"
+  },
+  "location": "Yammer Track",
+  "starting_at": "2013/05/29 11:30:00",
+  "ending_at": "2013/05/29 12:00:00"
+},
+{
+  "uid": "JSCONF2013-B104",
+  "title": "Your Function is Showing.",
+  "description": "As developers we spend quite a bit of time setting up routing between client and server components. With the advent of node and simple control flow mechanisms such as promises, we have the ability to easily expose JavaScript functions as remote procedure calls to the client. These simple patterns and helper libraries enable us to build extremely fast backends for our modern web applications.",
+  "speaker": {
+    "name": "Luis Montes",
+    "description": "@monteslu"
+  },
+  "location": "SpringSource Track",
+  "starting_at": "2013/05/29 11:30:00",
+  "ending_at": "2013/05/29 12:00:00"
+},
+
+
+
+
+{
+  "uid": "JSCONF2013-A105",
+  "title": "Learning New Words",
+  "description": "Godwin's Law for JavaScript: as people discuss a proposed extension to JavaScript syntax, the likelihood of someone saying "Stop trying to turn JavaScript into Java!" approaches 1.\nFor years I've tried to understand this perspective. Why are there so many people out there for whom present-day JavaScript is Good Enough™? What crucial aspect of JavaScript's identity are they trying to defend? Why do they get so scared when someone suggests new syntax? Can we figure out how to evolve the language without incurring the wrath of their pitchforks and/or torches?\nI'm going to spend half an hour trying to figure it out, and I invite you to join me.",
+  "speaker": {
+    "name": "Andrew Dupont",
+    "description": "@andrewdupont"
+  },
+  "location": "Yammer Track",
+  "starting_at": "2013/05/29 12:00:00",
+  "ending_at": "2013/05/29 12:30:00"
+},
+{
+  "uid": "JSCONF2013-B105",
+  "title": "Down The Rabbit Hole: Chasing bugs from the browser to the backend",
+  "description": " Oh no! You have a bug in your app, but you have no idea where it is. I’ll walk you through how we found and squashed a gnarly bug in socket.io using wireshark, chrome’s developer tools, lots of logging, and pretty graphs. I’ll also show you some good tips and tricks for tracking down and squashing bugs of your own.",
+  "speaker": {
+    "name": "Brandon Dimcheff",
+    "description": "@bdimcheff"
+  },
+  "location": "SpringSource Track",
+  "starting_at": "2013/05/29 12:00:00",
+  "ending_at": "2013/05/29 12:30:00"
+}
+,
+
+
+
+
+{
+  "uid": "JSCONF2013-A106",
+  "title": "#dadt (Dojo already did that)",
+  "description": "or: \"The underpinnings of a modern day web Application\". A quick, fun (and educational) look back over nearly a decade of Javascript evolution. Where we are now, where we started, and the mess of nonsense that happened in between.",
+  "speaker": {
+    "name": "Peter Higgins",
+    "description": "@phiggins"
+  },
+  "location": "Yammer Track",
+  "starting_at": "2013/05/29 13:30:00",
+  "ending_at": "2013/05/29 14:00:00"
+},
+{
+  "uid": "JSCONF2013-B106",
+  "title": "Performance Tuning Secrets",
+  "description": "Performance-tuning JavaScript code in a browser environment can feel like a dark art. Let's shine a spotlight on some tricks of the trade: from low-level Chrome tracing to high-speed cameras, and everything in between. We’ll also cover W3C proposals that may lead us to a brighter future -- and discuss when enough optimization is enough.",
+  "speaker": {
+    "name": "Peter Flynn",
+    "description": "@peterflynn"
+  },
+  "location": "SpringSource Track",
+  "starting_at": "2013/05/29 13:30:00",
+  "ending_at": "2013/05/29 14:00:00"
+},
+{
+  "uid": "JSCONF2013-C106",
+  "title": "WTF is \"this\"? 4 Simple Rules of a Crazy Keyword",
+  "description": "This intermediate talk will de-mystify the keyword 'this', and give you the 4 simple rules that govern it. If you've ever had trouble guessing what object "this" should refer to during a function call, we'll make sure you leave with a compass for figuring it out every time.",
+  "speaker": {
+    "name": "Marcus Philips",
+    "description": "@mracus"
+  },
+  "location": "Training Track",
+  "starting_at": "2013/05/29 13:30:00",
+  "ending_at": "2013/05/29 15:00:00"
+
+}
+
+
+];
